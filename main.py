eaf86a4f54d1716fc44984c92e9cec1c4a784eb9
nhalflants <natacha_halflants@hotmail.com>
2022-12-21 13:00:46 +0100

    Add Projects content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config("wide")
 
@@ -17,3 +18,21 @@ apps_intro = """
 Below you can find some of the apps I have built in Python. Feel free to contact me
 """
 st.info(apps_intro)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+project_col1, project_col2 = st.columns(2)
+
+with project_col1:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with project_col2:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
