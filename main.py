138c482cad0520980c9bac750ad1a604b2a6b685
nhalflants <natacha_halflants@hotmail.com>
2022-12-21 12:43:19 +0100

    Add Intro

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config("wide")
+
+st.title("Natacha Halflants")
+
+intro = """
+Hi, I am Natacha ! I am a python programmer. I love to learn and build apps. I am a driven Senior Functional Analyst 
+with an overall 9 years of IT experience in varied technologies and domains. I have a rich experience in analyzing 
+requirements, designing technical solution, creating specifications and ensuring development and implementation of web 
+and mobile solutions. Enriched exposure to third party services integration, api development and delivering financial 
+services interfaces. 
+"""
+st.write(intro)
+
+apps_intro = """
+Below you can find some of the apps I have built in Python. Feel free to contact me
+"""
+st.info(apps_intro)
